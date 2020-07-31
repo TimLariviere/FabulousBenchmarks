4a19b2a942ff08748d20070bcf94907e2d8f3def
Timothé Larivière <timothe.lariviere@outlook.com>
2020-07-31 12:02:19 +0200

    Benchmarks console

--- FabulousBenchmarks/App.fs
+++
@@ -1,9 +0,0 @@
-namespace FabulousBenchmarks
-
-open Xamarin.Forms
-
-type App() as this =
-    inherit Application()
-    
-    do this.MainPage <- MainPage()
-        
---
+++ FabulousBenchmarks/Program.fs
@@ -0,0 +1,41 @@
+﻿module Program
+
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+open Fabulous.XamarinForms
+open Xamarin.Forms
+       
+[<MemoryDiagnoser>]
+type ViewElementBenches() =
+    [<Benchmark>]
+    member self.EmptyDeclaration() =
+        [ for i in 1 .. 10000 ->
+            View.Label()
+        ]
+        
+    [<Benchmark>]
+    member self.SimpleDeclaration() =
+        [ for i in 1 .. 10000 ->
+            View.Label(
+                text = "Hello world",
+                fontSize = FontSize 22.,
+                textColor = Color.Black
+            )
+        ]
+        
+    [<Benchmark>]
+    member self.SimpleDeclarationWith() =
+        [ for i in 1 .. 10000 ->
+            View.Label(
+                text = "Hello world",
+                fontSize = FontSize 22.,
+                textColor = Color.Black
+            ).Row(1).Column(1)
+        ]
+       
+let defaultSwitch () = BenchmarkSwitcher [|typeof<ViewElementBenches> |]
+
+[<EntryPoint>]
+let Main args =
+    let summary = defaultSwitch().Run args
+    0
